cd79c274bbc83528f66deeecd3c7c5688ff3dff0
manhuakatalon <man.hua@katalon.com>
2024-11-27 12:46:44 +0700

    add ado test case

---
+++ Test Suites/ADO test suite.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ADO test suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>66e68948-9920-448c-9404-b0bf6c2f8e52</testSuiteGuid>
+   <testCaseLink>
+      <guid>1ecd7ea0-280e-4c74-bd33-637802ca10d5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Specific Test Cases</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>fcd6c78b-ea36-45f3-86a2-1a975357fe2a</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
--- Test Suites/healthcare-tests - TS_RegressionTest.ts
+++ Test Suites/healthcare-tests - TS_RegressionTest.ts
@@ -16,6 +16,7 @@
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Main Test Cases/TC1_Verify Successful Login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
       <variableLink>
          <testDataLinkId></testDataLinkId>
          <type>DEFAULT</type>
@@ -28,17 +29,5 @@
          <value></value>
          <variableId>a97344e7-5f9c-4936-92a2-d85e5463f3c9</variableId>
       </variableLink>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>e4bf2157-13c0-4c5f-875c-fa60a7e7a2fa</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Main Test Cases/TC2_Verify Successful Appointment</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>955abe25-4029-43e2-b02f-57315c1ade49</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Main Test Cases/TC3_Visual Testing Example</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
