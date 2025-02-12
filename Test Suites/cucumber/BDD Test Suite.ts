8f5f81d2662887c16bd0897fd49c7f6f9770161c
manhuakatalon <man.hua@katalon.com>
2025-02-12 15:33:08 +0700

    2 .prj in the same git

---
+++ Test Suites/cucumber/BDD Test Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>BDD Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>e3b2505c-3af7-4528-82e7-00fa34342af1</testSuiteGuid>
+</TestSuiteEntity>
