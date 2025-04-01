4aca2e1e69b78e58d75de8bb9db08738215a50c7
man.hua@katalon.com <katalon_g3_dummy_username>
2025-04-01 16:11:59 +0000

    Delete "Specific Test Cases (1)" test case

--- Test Suites/manhuaqeado0012.ts
+++ Test Suites/manhuaqeado0012.ts
@@ -1,27 +1,27 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
-   <description></description>
-   <name>manhuaqeado0012</name>
-   <tag></tag>
-   <isRerun>false</isRerun>
-   <mailRecipient></mailRecipient>
-   <numberOfRerun>3</numberOfRerun>
-   <pageLoadTimeout>30</pageLoadTimeout>
-   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
-   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>true</rerunImmediately>
-   <testSuiteGuid>286b2b0c-95ec-4616-960c-545d0f860290</testSuiteGuid>
-   <testCaseLink>
-      <guid>de898e69-ffb5-4067-8773-98e55d3bfcd5</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Specific Test Cases</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>fcd6c78b-ea36-45f3-86a2-1a975357fe2a</variableId>
-      </variableLink>
-   </testCaseLink>
+  <description/>
+  <name>manhuaqeado0012</name>
+  <tag/>
+  <isRerun>false</isRerun>
+  <mailRecipient/>
+  <numberOfRerun>3</numberOfRerun>
+  <pageLoadTimeout>30</pageLoadTimeout>
+  <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+  <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+  <rerunImmediately>true</rerunImmediately>
+  <testSuiteGuid>286b2b0c-95ec-4616-960c-545d0f860290</testSuiteGuid>
+  <testCaseLink>
+    <guid>de898e69-ffb5-4067-8773-98e55d3bfcd5</guid>
+    <isReuseDriver>false</isReuseDriver>
+    <isRun>true</isRun>
+    <testCaseId>Test Cases/Specific Test Cases</testCaseId>
+    <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+    <variableLink>
+      <testDataLinkId/>
+      <type>DEFAULT</type>
+      <value/>
+      <variableId>fcd6c78b-ea36-45f3-86a2-1a975357fe2a</variableId>
+    </variableLink>
+  </testCaseLink>
 </TestSuiteEntity>
